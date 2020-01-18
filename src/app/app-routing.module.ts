ec060a06195aabddcd4d59a76ba7019b74786042
danielsind <danielsindet@gmail.com>
2020-01-18 19:56:32 +0300

    Created a new component nav-bar --added the repo.component to the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RepoComponent} from '../app/repo/repo.component';
+import {GitComponent} from '../app/git/git.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'repo', component:RepoComponent},
+  {path:'git',component:GitComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { GitComponent } from './git/git.component';
 import { GitService } from './git/git.service';
 import { RepoComponent } from './repo/repo.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 const routes:Routes = [
   {path:"git",component:GitComponent},
@@ -18,7 +19,8 @@ const routes:Routes = [
   declarations: [
     AppComponent,
     GitComponent,
-    RepoComponent
+    RepoComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
