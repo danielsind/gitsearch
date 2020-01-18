81ee90eac50dc68c34e4cb74184e00f9976ea9eb
danielsind <danielsindet@gmail.com>
2020-01-18 10:37:30 +0300

    Created a git.service.ts file ,made it injectable and declared the properties to be used

---
+++ src/app/git/git.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class GitService {
+    private username: string;
+    private apiUrl: string = "https://api.github.com/users/";
+    private apiKey: string = environment.Git_Secret;
+}
