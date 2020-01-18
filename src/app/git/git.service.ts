5fc8a90e8079824bd77204f9626409c0f8a463f0
danielsind <danielsindet@gmail.com>
2020-01-18 13:49:45 +0300

    Added to the constructor private property http:HttpClient and another function updateUser whhich is used to show the new user name - attmpted to update angual-cli to fix .map bug, added an import from rjx all these on git.service.ts

--- src/app/git/git.service.ts
+++ src/app/git/git.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from "@angular/core";
 import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
+import  'rxjs/add/operator/map';
+import 'rxjs/Rx';
 
 @Injectable()
 export class GitService {
@@ -17,5 +19,12 @@ export class GitService {
   //Git Repos
   gitRepos() {
     return this.http.get(this.apiUrl + this.username + "/repos" + "?access_token=" + this.apiKey).map(result => result)
+  }
+  constructor(private http: HttpClient) {
+    console.log("GitService Ready");
+    this.username = "danielsind"
+  }
+  updateUser(username: string) {
+    this.username = username;
   }
 }
