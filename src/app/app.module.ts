8d0a2b8f5d4233628912233e9752330344e28e78
danielsind <danielsindet@gmail.com>
2020-01-18 10:29:48 +0300

    Create a new folder called git that contains git.component ts,css,html,spec files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitComponent } from './git/git.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitComponent
   ],
   imports: [
     BrowserModule,
