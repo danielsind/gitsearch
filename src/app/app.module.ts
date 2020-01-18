dad3d9b2811dc2268d934daf0135fc5df08eb731
danielsind <danielsindet@gmail.com>
2020-01-18 10:59:07 +0300

    Fixed the bug in git.service.ts - corrected the import{environment} from '../../environments/environmrnt'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { GitComponent } from './git/git.component';
 @NgModule({
   declarations: [
     AppComponent,
-    GitComponent
+    GitComponent,
+    
   ],
   imports: [
     BrowserModule,
