dbebb83267024b1cf3158bbdb39a7b37d6846af8
danielsind <danielsindet@gmail.com>
2020-01-18 13:52:26 +0300

    Generated a new component called repo with ts,html,css,and spec files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitComponent } from './git/git.component';
+import { RepoComponent } from './repo/repo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GitComponent,
+    RepoComponent,
 
   ],
   imports: [
